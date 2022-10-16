3efeae58a2a650144739581b6cff60071aebccaa
Levan Katsadze <leodevbro@gmail.com>
2022-10-16 14:38:41 +0400

    comm7

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ export const getStaticProps: GetStaticProps = async () => {
   await new Promise((res, rej) => {
     setTimeout(() => {
       res(true);
-    }, 15000);
+    }, 100);
   });
   console.log("end poks");
 
